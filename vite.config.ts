d402d67f1bcc28b9793951afe13f59f130078283
ouattara sotigui <ouattso192@gmail.com>
2026-06-10 11:44:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
-  vite: {
-    nitro: {
-      preset: "vercel",
-    },
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
